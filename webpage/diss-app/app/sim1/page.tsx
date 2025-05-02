20bb45bd9000aa60836d25dfe6f23d06fd235829
Quin Mundell <114661153+qumundell@users.noreply.github.com>
2025-05-02 10:55:37 +0100

    Added titles for web accessibilty

--- webpage/diss-app/app/sim1/page.tsx
+++ webpage/diss-app/app/sim1/page.tsx
@@ -2,6 +2,9 @@
 export default function Sim1() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+
+      <title>Simulation 1</title>
+=
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div>
             <h1>World 1 Simulation</h1>
--- webpage/diss-app/app/sim2/page.tsx
+++ webpage/diss-app/app/sim2/page.tsx
@@ -2,7 +2,9 @@
 export default function Sim2() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <title>Simulation 2</title>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        
         <div>
             <h1>World 2 Simulation</h1>
             <iframe
